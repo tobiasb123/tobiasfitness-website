c9b2c792017ea85bac34af09610ab2804e853dd3
Tobias Bastholm <tobias.bastholm@gmail.com>
2026-03-18 17:44:54 +0100

    Changed the file location of index.html, styles.scss and main.ts back to the original

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './src/app/app.config';
-import { App } from './src/app/app.component';
+import { appConfig } from './app/app.config';
+import { App } from './app/app.component';
 
 bootstrapApplication(App, appConfig).catch((err) => console.error(err));
